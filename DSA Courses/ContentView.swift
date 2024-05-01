462a93417af4d4e510d7772684edd5ee79db7d26
Nicolas Bouème <nicolasboueme@gmail.com>
2024-05-01 13:22:46 +0900

    Initial Commit

---
+++ DSA Courses/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DSA Courses
+//
+//  Created by Nicolas Bouème on 01/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DSA Courses/DSA_CoursesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSA_CoursesApp.swift
+//  DSA Courses
+//
+//  Created by Nicolas Bouème on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DSA_CoursesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
